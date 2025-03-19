74d32dede0d92f1e2a7612374d083d9d19ca6d50
iFreaku <90742387+iFreaku@users.noreply.github.com>
2025-03-19 19:42:27 +0530

    new

---
+++ app/main.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+
+fkey = os.environ.get('fxkey')
+
+@app.route('/')
+def home():
+    return fkey
+
+# Another example route
+@app.route('/about')
+def about():
+    return "This is the about page."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
